540e48d920246e67511e4a68c88490aac8b893d2
Fatikhova Guzel <nazar.fatikhov@gmail.com>
2022-08-29 10:58:18 +0300

    add folders

---
+++ Ex1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.WriteLine("Введите число, которое входит в рамки типа int");
+int m = int.Parse(Console.ReadLine());
+int n=m;
+int i = 0;
+while(n>0)
+{
+    n=n/10;
+    i=i+1;
+}
+
+Console.WriteLine(i + " цифр в числе");
+int[] arr = new int[i];
+
+n=m;
+for (int index=0; index<i; index++)
+{
+  arr[index]=n%10; 
+  n=n/10;
+}
+    bool b=false;
+for (int index=0; index<i/2; index++)
+{
+    if (arr[index]!=arr[i-1-index])
+    {
+        Console.WriteLine("No");
+        b=true;
+        break;
+    }
+}
+if (b==false)
+{
+    Console.WriteLine("Yes");
+}
+
+
+
---
+++ Ex3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64,125
+
+int[] GetSquares(int n){
+        int[] arr = new int[n];
+    for(int i=1;i<=n;i++){
+        arr[i-1]=i*i*i;
+    }
+       return arr;       
+}
+
+int n=10;
+int[] arr= GetSquares(n);
+for(int i=0;i<n;i++)
+{
+    if( i<n-1)  {
+    System.Console.Write(arr[i] + ", ");
+    }
+    else { 
+        System.Console.Write(arr[i] + "."); 
+    }
+}
